e05c3b8b3006cd5d96f88d6783b05af71935fe92
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-19 08:54:08 -0300

    onload por las dudas

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const btn = document.getElementById('btn');
-btn.addEventListener('click', () => {
-    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    getSum(nums);
-});
+
+window.onload = () => {
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+        let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        getSum(nums);
+    });    
+}
 
 
 /**
